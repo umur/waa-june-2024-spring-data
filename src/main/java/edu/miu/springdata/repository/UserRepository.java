abc4149b2b53c8d190859916df5af68a5f73e3b6
Suman Adhikari <expertxkid@gmail.com>
2024-06-27 11:29:01 -0500

    feat/SpringData: add findFirstByEmail(email) in UserRepository

--- src/main/java/edu/miu/springdata/repository/UserRepository.java
+++ src/main/java/edu/miu/springdata/repository/UserRepository.java
@@ -3,5 +3,8 @@ package edu.miu.springdata.repository;
 import edu.miu.springdata.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findFirstByEmail(String email);
 }
