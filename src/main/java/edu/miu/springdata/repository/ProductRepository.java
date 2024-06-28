47edc795a3e091d6e9b317214501c3e9c9991a32
Suman Adhikari <expertxkid@gmail.com>
2024-06-27 22:16:27 -0500

    feat/SpringData: add new methods for assignments in ProductRepository

--- src/main/java/edu/miu/springdata/repository/ProductRepository.java
+++ src/main/java/edu/miu/springdata/repository/ProductRepository.java
@@ -1,10 +1,19 @@
 package edu.miu.springdata.repository;
 
 import edu.miu.springdata.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findDistinctFirstByCategoryId(Long categoryId);
+
+    Page<Product> findAllByPriceGreaterThanEqual(BigDecimal price, Pageable pageable);
+
+    Page<Product> findAllByPriceLessThanEqualAndCategoryId(BigDecimal price, Long categoryId, Pageable pageable);
+
+    Page<Product> findAllByNameContaining(String name, Pageable pageable);
 }
