7bcb23c8d5d4ad56b5b551caaf0d682d4c3f05f6
Suman Adhikari <expertxkid@gmail.com>
2024-06-27 21:28:27 -0500

    feat/SpringData: add findDistinctFirstByCategoryId(catId) in ProductRepository

--- src/main/java/edu/miu/springdata/repository/ProductRepository.java
+++ src/main/java/edu/miu/springdata/repository/ProductRepository.java
@@ -1,10 +1,10 @@
 package edu.miu.springdata.repository;
 
 import edu.miu.springdata.domain.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findAllByCategoryId(Long categoryId, Pageable pageable);
+    Optional<Product> findDistinctFirstByCategoryId(Long categoryId);
 }
