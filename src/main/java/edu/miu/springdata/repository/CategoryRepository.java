6325b78fd990dfcec1316b764a7916a08f7dd472
Suman Adhikari <expertxkid@gmail.com>
2024-06-27 21:29:01 -0500

    feat/SpringData: add findByName(name) in CategoryRepository

--- src/main/java/edu/miu/springdata/repository/CategoryRepository.java
+++ src/main/java/edu/miu/springdata/repository/CategoryRepository.java
@@ -2,11 +2,9 @@ package edu.miu.springdata.repository;
 
 import edu.miu.springdata.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    @Query("SELECT c FROM Category c JOIN FETCH c.products p WHERE p.id = :productId")
-    Category findByProductId(@Param("productId") Long productId);
+    Optional<Category> findByName(String name);
 }
